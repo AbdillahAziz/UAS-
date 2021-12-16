53c7303c62f8de0fedfb57155c716aacf339667d
AbdillahAziz <96118145+AbdillahAziz@users.noreply.github.com>
2021-12-16 15:53:50 +0700

    Update UAS_Statistik_Produksi_Minyak.py

--- UAS_Statistik_Produksi_Minyak.py
+++ UAS_Statistik_Produksi_Minyak.py
@@ -13,8 +13,6 @@ from matplotlib import cm
 import streamlit as st
 from PIL import Image
 
-[theme] 
-base = "dark"
 #>>>>>>>>>>>> title <<<<<<<<<<<<#
 st.set_page_config(layout="wide")  # this needs to be the first Streamlit command called
 st.title("Statistik Produksi Minyak Berbagai Negara")
